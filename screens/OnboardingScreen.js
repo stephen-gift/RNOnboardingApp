970c2fd73ddf47df835fdf2097e303a59aaf223e
Stephen <79639335+stephen-gift@users.noreply.github.com>
2024-02-06 16:31:08 +0100

    Onboarding Pages

--- screens/OnboardingScreen.js
+++ screens/OnboardingScreen.js
@@ -1,12 +1,49 @@
 import { Dimensions, SafeAreaView, StyleSheet, Text, View } from "react-native";
 import React from "react";
 
+import Onboarding from "react-native-onboarding-swiper";
+
 const { width, height } = Dimensions.get("window");
 
 const OnboardingScreen = () => {
   return (
     <SafeAreaView style={styles.container}>
-      <Text>OnboardingScreen</Text>
+      <Onboarding
+        containerStyles={{ paddingHorizontal: 15 }}
+        pages={[
+          {
+            backgroundColor: "#a7f3d0",
+            image: (
+              <View>
+                <Text>Onboarding</Text>
+              </View>
+            ),
+            title: "Boost Productivity",
+            subtitle: "Subscribe this channel to boost your productivity level",
+          },
+          {
+            backgroundColor: "#fef3c7",
+            image: (
+              <View>
+                <Text>Onboarding</Text>
+              </View>
+            ),
+            title: "Work Seamlessly",
+            subtitle: "Get your work done seamlessly without interruption",
+          },
+          {
+            backgroundColor: "#a78bfa",
+            image: (
+              <View>
+                <Text>Onboarding</Text>
+              </View>
+            ),
+            title: "Achieve Higher Goals",
+            subtitle:
+              "By boosting your productivity we help you to achieve higher goals",
+          },
+        ]}
+      />
     </SafeAreaView>
   );
 };
