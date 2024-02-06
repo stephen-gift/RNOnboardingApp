8065f551fad86f8c1a0484af9e2d5d44f0056e0d
Stephen <79639335+stephen-gift@users.noreply.github.com>
2024-02-06 17:42:29 +0100

    update

--- navigation/AppNavigation.js
+++ navigation/AppNavigation.js
@@ -1,29 +1,70 @@
 import { StyleSheet, Text, View } from "react-native";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import HomeScreen from "../screens/HomeScreen";
 import OnboardingScreen from "../screens/OnboardingScreen";
+import { getData } from "../utils/asyncStorage";
 
 const Stack = createNativeStackNavigator();
 
 const AppNavigation = () => {
-  return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="Onboarding">
-        <Stack.Screen
-          name="Onboarding"
-          options={{ headerShown: false }}
-          component={OnboardingScreen}
-        />
-        <Stack.Screen
-          name="Home"
-          options={{ headerShown: false }}
-          component={HomeScreen}
-        />
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
+  const [showOnboarding, setShowOnboarding] = useState(null);
+
+  useEffect(() => {
+    checkIfAlreadyOnboarded();
+  }, []);
+
+  const checkIfAlreadyOnboarded = async () => {
+    let onboarded = await getData("onboarded");
+    if (onboarded == 1) {
+      //   HIDE ONBOARDING
+      setShowOnboarding(false);
+    } else {
+      // SHOW ONBOARDING
+      setShowOnboarding(true);
+    }
+  };
+
+  if (showOnboarding == null) {
+    return null;
+  }
+
+  if (showOnboarding) {
+    return (
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="Onboarding">
+          <Stack.Screen
+            name="Onboarding"
+            options={{ headerShown: false }}
+            component={OnboardingScreen}
+          />
+          <Stack.Screen
+            name="Home"
+            options={{ headerShown: false }}
+            component={HomeScreen}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
+    );
+  } else {
+    return (
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="Home">
+          <Stack.Screen
+            name="Onboarding"
+            options={{ headerShown: false }}
+            component={OnboardingScreen}
+          />
+          <Stack.Screen
+            name="Home"
+            options={{ headerShown: false }}
+            component={HomeScreen}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
+    );
+  }
 };
 
 export default AppNavigation;
--- screens/OnboardingScreen.js
+++ screens/OnboardingScreen.js
@@ -10,6 +10,7 @@ import React from "react";
 import Onboarding from "react-native-onboarding-swiper";
 import LottieView from "lottie-react-native";
 import { useNavigation } from "@react-navigation/native";
+import { storeData } from "../utils/asyncStorage";
 
 const { width, height } = Dimensions.get("window");
 
@@ -18,6 +19,7 @@ const OnboardingScreen = () => {
 
   const handleDone = () => {
     navigation.navigate("Home");
+    storeData("onboarded", "1");
   };
 
   const doneButton = ({ ...props }) => {
