c814311620a14723d4c248fc969a072c6d3f75b8
Stephen <79639335+stephen-gift@users.noreply.github.com>
2024-02-06 17:17:39 +0100

    HomeScreen

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,15 +1,55 @@
-import { StyleSheet, Text, View } from "react-native";
+import {
+  Dimensions,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
 import React from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
+import LottieView from "lottie-react-native";
+
+const { width, height } = Dimensions.get("window");
+
+const handleReset = async () => {
+  navigation.push("Onboarding");
+};
 
 const HomeScreen = () => {
   return (
-    <SafeAreaView>
-      <Text>HomeScreen</Text>
+    <SafeAreaView style={styles.container}>
+      <LottieView
+        style={styles.lottie}
+        source={require("../assets/animations/confetti.json")}
+        autoPlay
+        loop
+      />
+      <Text style={styles.text}>Home Page</Text>
+      <TouchableOpacity onPress={handleReset} style={styles.resetButton}>
+        <Text>Reset</Text>
+      </TouchableOpacity>
     </SafeAreaView>
   );
 };
 
 export default HomeScreen;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+  },
+  lottie: {
+    width: width * 0.9,
+    height: width,
+  },
+  text: {
+    fontSize: width * 0.09,
+    marginBottom: 20,
+  },
+  resetButton: {
+    backgroundColor: "#34d399",
+    padding: 10,
+    borderRadius: 10,
+  },
+});
