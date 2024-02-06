d158958e7b4ba6c6e5a7d62e8ad060dba94d5d98
Stephen <79639335+stephen-gift@users.noreply.github.com>
2024-02-06 16:15:35 +0100

    update

--- navigation/AppNavigation.js
+++ navigation/AppNavigation.js
@@ -2,6 +2,8 @@ import { StyleSheet, Text, View } from "react-native";
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import HomeScreen from "../screens/HomeScreen";
+import OnboardingScreen from "../screens/OnboardingScreen";
 
 const Stack = createNativeStackNavigator();
 
