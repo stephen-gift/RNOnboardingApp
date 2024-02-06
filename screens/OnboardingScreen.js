ff82abf5330aae0cec0020b8ed808db436cc49c3
Stephen <79639335+stephen-gift@users.noreply.github.com>
2024-02-06 16:22:23 +0100

    update and onboarding installation

--- screens/OnboardingScreen.js
+++ screens/OnboardingScreen.js
@@ -1,9 +1,11 @@
-import { SafeAreaView, StyleSheet, Text, View } from "react-native";
+import { Dimensions, SafeAreaView, StyleSheet, Text, View } from "react-native";
 import React from "react";
+
+const { width, height } = Dimensions.get("window");
 
 const OnboardingScreen = () => {
   return (
-    <SafeAreaView>
+    <SafeAreaView style={styles.container}>
       <Text>OnboardingScreen</Text>
     </SafeAreaView>
   );
@@ -11,4 +13,19 @@ const OnboardingScreen = () => {
 
 export default OnboardingScreen;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "white",
+  },
+  lottie: {
+    width: width * 0.9,
+    height: width,
+  },
+  doneButton: {
+    padding: 20,
+    // backgroundColor: 'white',
+    // borderTopLeftRadius: '100%',
+    // borderBottomLeftRadius: '100%'
+  },
+});
